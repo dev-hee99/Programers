cf06c4bfeef965bc868288d54f70b69636fb60a1
Changhee99 <70744371+Changhee99@users.noreply.github.com>
2021-07-06 19:16:55 +0900

    level1

--- study/src/level1/phoneketmon.java
+++ study/src/level1/phoneketmon.java
@@ -1,11 +1,18 @@
 package level1;
 
-public class phoneketmon {
-    public static void main(String[] args) {
+import java.util.*;
+import java.util.stream.Collectors;
 
+public class phoneketmon {
+    private static int[] arr = {3,3,1,2};
+    public static void main(String[] args) {
+       System.out.println(solution(arr));
     }
     private static int solution(int[] nums) {
         int answer = 0;
-        return answer;
+        Set set = new HashSet<Integer>();
+        set = Arrays.stream(nums).boxed().collect(Collectors.toSet());
+
+        return nums.length/2 > set.size() ? set.size() : nums.length/2;
     }
 }
