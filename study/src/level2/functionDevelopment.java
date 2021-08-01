33c6d403c812d642d204e1a2e5fafbd90418d474
Changhee99 <70744371+Changhee99@users.noreply.github.com>
2021-08-02 03:03:41 +0900

    기능개발

--- study/src/level2/functionDevelopment.java
+++ study/src/level2/functionDevelopment.java
@@ -1,5 +1,7 @@
 package level2;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Queue;
 
 /*
@@ -17,25 +19,61 @@ import java.util.Queue;
 작업 속도는 100 이하의 자연수입니다.
 배포는 하루에 한 번만 할 수 있으며, 하루의 끝에 이루어진다고 가정합니다. 예를 들어 진도율이 95%인 작업의 개발 속도가 하루에 4%라면 배포는 2일 뒤에 이루어집니다.
 입출력 예
-progresses	speeds	return
-[93, 30, 55]	[1, 30, 5]	[2, 1]
+progresses	                speeds	            return
+[93, 30, 55]	            [1, 30, 5]	        [2, 1]
 [95, 90, 99, 99, 80, 99]	[1, 1, 1, 1, 1, 1]	[1, 3, 2]
 입출력 예 설명
 입출력 예 #1
 첫 번째 기능은 93% 완료되어 있고 하루에 1%씩 작업이 가능하므로 7일간 작업 후 배포가 가능합니다.
-두 번째 기능은 30%가 완료되어 있고 하루에 30%씩 작업이 가능하므로 3일간 작업 후 배포가 가능합니다. 하지만 이전 첫 번째 기능이 아직 완성된 상태가 아니기 때문에 첫 번째 기능이 배포되는 7일째 배포됩니다.
+두 번째 기능은 30%가 완료되어 있고 하루에 30%씩 작업이 가능하므로 3일간 작업 후 배포가 가능합니다.
+하지만 이전 첫 번째 기능이 아직 완성된 상태가 아니기 때문에 첫 번째 기능이 배포되는 7일째 배포됩니다.
 세 번째 기능은 55%가 완료되어 있고 하루에 5%씩 작업이 가능하므로 9일간 작업 후 배포가 가능합니다.
 
 따라서 7일째에 2개의 기능, 9일째에 1개의 기능이 배포됩니다.
 
 입출력 예 #2
-모든 기능이 하루에 1%씩 작업이 가능하므로, 작업이 끝나기까지 남은 일수는 각각 5일, 10일, 1일, 1일, 20일, 1일입니다. 어떤 기능이 먼저 완성되었더라도 앞에 있는 모든 기능이 완성되지 않으면 배포가 불가능합니다.
+모든 기능이 하루에 1%씩 작업이 가능하므로, 작업이 끝나기까지 남은 일수는 각각 5일, 10일, 1일, 1일, 20일, 1일입니다.
+ 어떤 기능이 먼저 완성되었더라도 앞에 있는 모든 기능이 완성되지 않으면 배포가 불가능합니다.
 
 따라서 5일째에 1개의 기능, 10일째에 3개의 기능, 20일째에 2개의 기능이 배포됩니다.
 
  */
 public class functionDevelopment {
-    public static void main(String[] args) {
+    private final static int[] progresses = {93, 30, 55};
+    private final static int[] speeds = {1, 30, 5};
 
+    public static void main(String[] args) {
+        for (int i : solution(progresses, speeds))
+            System.out.println(i);
+    }
+
+    public static int[] solution(int[] progresses, int[] speeds) {
+        List<Integer> list = new ArrayList<>();
+        int index = 0; //작업진행도
+        int cnt = 0; //일별 완료 작업 수
+
+        while (index < progresses.length) {
+            cnt = 0;
+            //하루 동안 증가
+            for (int i = index; i < progresses.length; i++) {
+                //완료시 진행X
+                if(progresses[i]>=100) continue;
+                progresses[i] += speeds[i];
+            }
+
+            for (int i = index; i < progresses.length; i++) {
+                //선행 작업이 완료되지 않을 경우 배포X
+                if (progresses[i] < 100) break;
+                if (progresses[i] >= 100) cnt++;
+            }
+
+            //완료 작업이 있을경우
+            if (cnt != 0) {
+                list.add(cnt);
+                index += cnt;
+            }
+        }
+
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
